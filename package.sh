0c7526fb455b42ed826c58e6ddb7cbb03dabb59b
Rachel <rachel87mul@gmail.com>
2022-10-25 22:06:27 -0500

    package install

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+    #Author : Rachel
+    #Date : Oct-25-2022
+
+## ---------- script that Install some packages in Linux -----------------
+
+yum install finger -y
+yum install gcc -y
+yum install make -y
+yum install python3 -y
+yum install epel-release -y
+yum install git -y
+
+echo "installation is now completed"
